c4cbf5098ee88df87be078cd04ef0184bda52500
ellen660 <ellenzhang660@gmail.com>
2022-01-15 00:17:58 -0500

    hi

---
+++ client/src/components/modules/Login.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import GoogleLogin, { GoogleLogout } from "react-google-login";
+
+import "../../utilities.css";
+import "../modules/Login.css";
+
+//TODO: REPLACE WITH YOUR OWN CLIENT_ID
+const GOOGLE_CLIENT_ID = "379531489685-7mgu977nsn7s5ei091hilu992i1vnch8.apps.googleusercontent.com";
+
+const Login = ({ userId, handleLogin, handleLogout }) => {
+  return (
+    <>
+      {userId ? (
+        <GoogleLogout
+          clientId={GOOGLE_CLIENT_ID}
+          buttonText="Logout"
+          onLogoutSuccess={handleLogout}
+          onFailure={(err) => console.log(err)}
+        />
+      ) : (
+        <GoogleLogin
+          clientId={GOOGLE_CLIENT_ID}
+          render={(renderProps) => (
+            <button className="Login-button" onClick={renderProps.onClick}>
+              {" "}
+              Login{" "}
+            </button>
+          )}
+          buttonText="Login"
+          onSuccess={handleLogin}
+          onFailure={(err) => console.log(err)}
+        />
+      )}
+    </>
+  );
+};
+
+export default Login;
