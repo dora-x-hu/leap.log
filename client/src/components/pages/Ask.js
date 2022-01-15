db591c6730709185317785458439e80146f139aa
Dora Hu <dorahu@dhcp-10-29-66-171.dyn.mit.edu>
2022-01-14 21:46:40 -0500

    added form on Ask page

--- client/src/components/pages/Ask.js
+++ client/src/components/pages/Ask.js
@@ -4,7 +4,14 @@ import { Router } from "@reach/router";
 const Ask = (props) => {
   return (
     <>
-      <div>there will be questions here i promise</div>
+      <form>
+        <section>
+          <label>journal prompt</label>
+        </section>
+
+        <input type="text"></input>
+        <input type="submit" value="Submit"></input>
+      </form>
     </>
   );
 };
