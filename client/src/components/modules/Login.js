d4793d624e82f8f75e545136a55f5446123b94f7
Michelle Chan <michellechan@C02FK92KMD6M.tld>
2022-01-16 12:47:47 -0500

    google logo

--- client/src/components/modules/Login.js
+++ client/src/components/modules/Login.js
@@ -23,7 +23,7 @@ const Login = ({ userId, handleLogin, handleLogout }) => {
           render={(renderProps) => (
             <button className="Login-button" onClick={renderProps.onClick}>
               {" "}
-              login with google{" "}
+              <img src="https://www.designbust.com/download/1016/png/google_logo_png_transparent512.png" width="30px" height="30px"></img> login with google{" "}
             </button>
           )}
           buttonText="Login"
