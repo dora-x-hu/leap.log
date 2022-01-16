a0bfea411b6569a9e9943225a36b128373fad35e
ellen660 <ellenzhang660@gmail.com>
2022-01-16 13:53:23 -0500

    stuff

--- client/src/components/pages/Day.js
+++ client/src/components/pages/Day.js
@@ -4,23 +4,22 @@ import SingleEntry from "../modules/SingleEntry.js";
 
 import { get } from "../../utilities";
 
-//parames: userid
-
 const Day = (props) => {
   const [entries, setEntries] = useState([]);
   //const [date, setDate] = useState();
 
   useEffect(() => {
-    get("/api/responses", { day: Date.now() }).then((responsesObj) => {
+    get("/api/responses", { day: 0, month: 0, year: 122 }).then((responsesObj) => {
       setEntries(responsesObj);
     });
+    //print(entries);
   }, []);
 
-  let entriesList = [];
+  let entriesList = null;
   const hasEntries = entries.length !== 0;
 
-<<<<<<< HEAD
-=======
+  //let entriesList = ["here", "there", "everywhere"];
+
   if (hasEntries) {
     entriesList = entries.map((responseObj) => (
       <SingleEntry
@@ -28,30 +27,58 @@ const Day = (props) => {
         content={responseObj.content}
         user_id={props.userId}
         day={responseObj.day}
+        month={responseObj.month}
+        year={responseObj.year}
       />
     ));
   } else {
     entriesList = "Start Journalling!";
   }
 
->>>>>>> 7d4559b51d243dc46c4ea6798ea93cbea305b6d6
   return (
-    <div>
-      {Date.now()}
-      <div>
-        {hasEntries
-          ? props.journalEntry.map((response) => {
-              <SingleEntry
-                question={response.question}
-                content={response.content}
-                user_id={props.userId}
-                day={response.day}
-              />;
-            })
-          : entriesList.concat("Start Journaling")}
-      </div>
-    </div>
+    //<section>{date}</section>
+    //<section>{entries}</section>
+    <section>{entriesList}</section>
   );
 };
 
 export default Day;
+
+//   if (hasEntries) {
+//     console.log("usess")
+//     entriesList = entries.map((responseObj) => (
+//       <SingleEntry
+//         question="question" //{responseObj.question}
+//         content="content" //{responseObj.content}
+//         user_id={props.userId}
+//         day="idk" //{responseObj.day}
+//       />
+//     ));
+//   } else {
+//     entriesList = "Start Journalling!";
+//   }
+
+//   return (
+//     //<section>{date}</section>
+//     //<section>{entriesList}</section>
+//     <div>
+//       {("Date: ", new Date())}
+//       <div>
+//         {hasEntries
+//           ? entriesList.concat(
+//               entries.map((response) => {
+//                 <SingleEntry
+//                   question={response.question}
+//                   content={response.content}
+//                   user_id={props.userId}
+//                   day={response.day}
+//                 />;
+//               })
+//             )
+//           : entriesList.concat("Start Journaling")}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Day;
