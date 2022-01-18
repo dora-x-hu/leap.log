86566ab9e6999a5687a4bed257870b10a8b6a7c6
ellen660 <ellenzhang660@gmail.com>
2022-01-18 14:24:07 -0500

    new headerfor Month

--- client/src/components/pages/Month.js
+++ client/src/components/pages/Month.js
@@ -78,16 +78,96 @@ const Month = (props) => {
 
   //useEFfects
 
+  const getMonthDetails = () => {
+    switch (d.getMonth()) {
+      case 0:
+        return {
+          name: "january",
+          days: 31,
+        };
+      case 1:
+        return {
+          name: "february",
+          days: isLeapYear() ? 29 : 28,
+        };
+      case 2:
+        return {
+          name: "march",
+          days: 31,
+        };
+      case 3:
+        return {
+          name: "april",
+          days: 30,
+        };
+      case 4:
+        return {
+          name: "may",
+          days: 31,
+        };
+      case 5:
+        return {
+          name: "june",
+          days: 30,
+        };
+      case 6:
+        return {
+          name: "july",
+          days: 31,
+        };
+      case 7:
+        return {
+          name: "august",
+          days: 31,
+        };
+      case 8:
+        return {
+          name: "septembe",
+          days: 30,
+        };
+      case 9:
+        return {
+          name: "october",
+          days: 31,
+        };
+      case 10:
+        return {
+          name: "november",
+          days: 30,
+        };
+      case 11:
+        return {
+          name: "december",
+          days: 31,
+        };
+    }
+  };
+
   return (
     <>
-      <div>
-        <button onClick={moveLeft}>Last Month</button>
+      <div className="month">
+        <ul>
+          <li className="prev" onClick={moveLeft}>
+            &#10094;
+          </li>
+          <li className="next" onClick={moveRight}>
+            &#10095;
+          </li>
+          <li className="title">
+            {getMonthDetails().name}
+            <br></br>
+            <span>{d.getFullYear()}</span>
+          </li>
+        </ul>
+      </div>
+      {/* <div>
+        <button onClick={moveLeft}>Last Month{console.log(d)}</button>
 
         <button onClick={moveRight}>
           Next Month
           {/* {String(d.getDate()) + "/" + String(d.getMonth()) + "/" + String(d.getFullYear())} */}
-        </button>
-      </div>
+      {/* </button>
+      </div> */}{" "}
       <div>
         {" "}
         <MonthGrid
