109ed1b761a2d834a189d8bace1c73fffc7d8f5d
ellen660 <ellenzhang660@gmail.com>
2022-01-20 22:34:47 -0500

    new change

--- client/src/components/pages/Day.js
+++ client/src/components/pages/Day.js
@@ -6,9 +6,9 @@ import { get } from "../../utilities";
 import { Link } from "@reach/router";
 
 const Day = (props) => {
-  console.log(props.currentYear);
-  console.log(props.currentMonth);
-  console.log(props.currentDate);
+  console.log(props.year);
+  console.log(props.month);
+  console.log(props.day);
 
   const [entries, setEntries] = useState([]);
   const [d, setDate] = useState(
@@ -16,7 +16,8 @@ const Day = (props) => {
   );
   const [entriesList, setEntriesList] = useState(null);
   const [promptsList, setPromptsList] = useState([]);
-  const current = new Date();
+  const newcurrent = new Date();
+  const current = new Date(newcurrent.getFullYear(), newcurrent.getMonth(), newcurrent.getDate());
 
   const moveRight = () => {
     let tomorrow = new Date(props.year, props.month - 1, props.day + 1);
@@ -96,67 +97,18 @@ const Day = (props) => {
   //   console.log(entriesList);
   // }, [entriesList]);
 
-  if (!props.userId) {
+  if (props.userId === "undefined") {
     return (
       <>
         <div className="Day-heading1">log in before using journal</div>
       </>
     );
-  } /*else if (entries.length !== 0) {
-    setEntriesList(
-      entries.map((responseObj) => (
-        <SingleEntryc
-          question={responseObj.question}
-          content={responseObj.content}
-          user_id={props.userId}
-          day={responseObj.day}
-          month={responseObj.month}
-          year={responseObj.year}
-          userId={props.userId}
-        />
-      ))
-    );
-    return (
-      <>
-        <section>
-          {String(d.getDate()) + "/" + String(d.getMonth() + 1) + "/" + String(d.getFullYear())}
-        </section>
-        <div>
-          <button onClick={() => moveRight()}>Next Day</button>
-          <button onClick={() => moveLeft()}>Previous Day</button>
-          <section>{entriesList}</section>
-        </div>
-      </>
-    );
-  }*/ else {
-    // return (
-    //   <>
-    //     <section className="Day-paragraph">
-    //       {String(String(d.getMonth() + 1) + "/" + d.getDate()) + "/" + String(d.getFullYear())}
-    //     </section>
-    //     <div className="Day-paragraph">
-    //       <button className="Day-button" onClick={() => moveLeft()}>
-    //         Previous Day
-    //       </button>
-    //       <button className="Day-button" onClick={() => moveRight()}>
-    //         Next Day
-    //       </button>
-    //     </div>
-    //     <div>
-    //       <DayGrid
-    //         day={d.getDate()}
-    //         month={d.getMonth()}
-    //         year={d.getFullYear()}
-    //         userId={props.userId}
-    //       />
-    //     </div>
-    //   </>
-    // );
-    if (entriesList === "" && current < new Date(props.year, props.month - 1, props.day)) {
+  } else {
+    if (current < new Date(props.year, props.month - 1, props.day)) {
       return (
         <div className="Day-heading1">
-          {/* {console.log(current)}
-          {console.log(new Date(props.year, props.month - 1, props.day))} */}
+          {console.log(current)}
+          {console.log(new Date(props.year, props.month - 1, props.day))}
           Not this date yet!
           <section className="Day-paragraph">
             {String(String(props.month) + "/" + props.day + "/" + String(props.year))}
@@ -182,8 +134,8 @@ const Day = (props) => {
     } else if (entriesList === "") {
       return (
         <>
-          {/* {console.log(current)}
-          {console.log(new Date(props.year, props.month - 1, props.day))}{" "} */}
+          {console.log(current)}
+          {console.log(new Date(props.year, props.month - 1, props.day))}{" "}
           <section className="Day-paragraph">
             {String(String(props.month) + "/" + props.day + "/" + String(props.year))}
           </section>
@@ -218,8 +170,8 @@ const Day = (props) => {
     } else {
       return (
         <>
-          {/* {console.log(current)}
-          {console.log(new Date(props.year, props.month - 1, props.day))} */}
+          {console.log(current)}
+          {console.log(new Date(props.year, props.month - 1, props.day))}
 
           <section className="Day-paragraph">
             {String(String(props.month) + "/" + props.day + "/" + String(props.year))}
